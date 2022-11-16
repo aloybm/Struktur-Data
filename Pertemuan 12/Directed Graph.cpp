56439896e4ff021ee8cf9fa9a1b26a570e40a720
aloybm <100351038+aloybm@users.noreply.github.com>
2022-11-16 10:40:35 +0700

    Update Directed Graph.cpp

--- Pertemuan 12/Directed Graph.cpp
+++ Pertemuan 12/Directed Graph.cpp
@@ -1,6 +1,3 @@
-// Program to print BFS traversal from a given
-// source vertex. BFS(int s) traverses vertices
-// reachable from s.
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -40,18 +37,15 @@ class Graph
 		}
 };
 
-
-// Driver program to test methods of graph class
 int main()
 {
-	// Create a graph given in the above diagram
 	Graph g(5);
 	g.addEdge(4, 2);
-    g.addEdge(4, 3);
-    g.addEdge(2, 1);
-    g.addEdge(2, 3);
-    g.addEdge(1, 3);
-    g.addEdge(0, 1);
+    	g.addEdge(4, 3);
+    	g.addEdge(2, 1);
+    	g.addEdge(2, 3);
+    	g.addEdge(1, 3);
+    	g.addEdge(0, 1);
 
 	cout << "Following is Breadth First Traversal "
 		<< "(starting from vertex 4) \n";
